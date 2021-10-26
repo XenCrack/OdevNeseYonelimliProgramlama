2795a72b367f889ba2609d7421b6d418f33f7db2
XenCrack <ouz1401@hotmail.com>
2021-10-26 12:00:28 +0300

    Add project files.

---
+++ OdevNeseYonelimliProgramlama/HesapMak.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdevNeseYonelimliProgramlama
+{
+    public static class HesapMak
+    {
+        public static void HesapMakinası(double sayi1, double sayi2, double sonuc)
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("lütfen işlem seçin....");
+            Console.WriteLine("1.    Toplam (+)");
+            Console.WriteLine("2.    Çıkarma(-)");
+            Console.WriteLine("3.    Çarpma (*)");
+            Console.WriteLine("4.    Bölme  (/)");
+            Console.WriteLine("******************************");
+            Console.Write("Lütfen işlemin sembolünü yazınız: ");
+            string islem = Console.ReadLine();
+
+            double sonuc = 0;
+
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            if (islem == "+")
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (islem == "-")
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (islem == "*")
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (islem == "/")
+            {
+                sonuc = sayi1 / sayi2;
+            }
+
+            switch (islem)
+            {
+                case "+":
+                    Console.Write($"sonuc ={sayi1 + sayi2}");
+                    break;
+                case "-":
+                    Console.Write($"sonuc ={sayi1 - sayi2}");
+                    break;
+                case "*":
+                    Console.Write($"sonuc ={sayi1 * sayi2}");
+                    break;
+                case "/":
+                    Console.Write($"sonuc ={sayi1 / sayi2}");
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("******************************");
+            Console.Read();
+        }
+
+        
+    }
+}
---
+++ OdevNeseYonelimliProgramlama/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace OdevNeseYonelimliProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Okul.Okul okul = new Okul.Okul();
+            okul.Sınıf();
+            
+            
+            Okul.Sınıf sınıf = new Okul.Sınıf();
+            sınıf.Ogrenci();
+
+            Matemtik.Mat mat = new Matemtik.Mat();
+            mat.Mate();
+
+            Matemtik.Mate2 mat2 = new Matemtik.Mate2();
+            mat2.Matek();
+
+            //****** hesap makinesini çalıştırmaya çalıştım çalıştıramadım******
+
+           /* HesapMak.HesapMakinası hes = new HesapMak.HesapMakinası();
+            hes.*/
+            
+
+
+            
+            Console.Read();
+
+        }
+    }
+
+    
+}
+namespace Okul
+{
+   public class Okul
+   {
+        public void Sınıf()
+        {
+            Console.WriteLine("");
+        }
+   }
+
+   public class Sınıf:Okul
+   {
+        public void Ogrenci()
+        {
+            Console.WriteLine("Okul > Sınıf > Ogrenci");
+        }
+   }
+
+    
+}
+
+namespace Matemtik
+{
+    public class Mat:Mate2
+    {
+        public void Mate()
+        {
+
+            Console.WriteLine("İki değer giriniz :");
+            double deger1 = Convert.ToDouble(Console.ReadLine());
+            double deger2 = Convert.ToDouble(Console.ReadLine());
+
+            double sonuc = deger1 + deger2;
+            Console.WriteLine($"sonuc : " + sonuc);
+
+        }
+    }
+
+    public class Mate2
+    {
+       public void Matek()
+        {
+            Console.WriteLine("İki değer giriniz :");
+            double deger1 = Convert.ToDouble(Console.ReadLine());
+            double deger2 = Convert.ToDouble(Console.ReadLine());
+
+            double sonuc = deger1 - deger2;
+            Console.WriteLine($"sonuc : " + sonuc);
+        }
+    }
+       
+
+
+}
+
+
